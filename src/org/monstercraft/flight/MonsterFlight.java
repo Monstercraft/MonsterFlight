7a0b4bf32e27c41503abe525405dffb767e23739
Fletch to 99 <fletchto99@hotmail.com>
2012-10-01 21:11:20 -0400

    Initial commit

---
+++ src/org/monstercraft/flight/MonsterFlight.java
@@ -0,0 +1,63 @@
+package org.monstercraft.flight;
+
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MonsterFlight extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        try {
+            new Metrics(this).start();
+        } catch (final IOException e) {
+            // Aww no metrics :(
+        }
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerRespawn(final PlayerRespawnEvent event) {
+        setSurvivalFlightMode(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerChangedWorld(final PlayerChangedWorldEvent event) {
+        setSurvivalFlightMode(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerGameModeChange(final PlayerGameModeChangeEvent event) {
+        setSurvivalFlightMode(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        setSurvivalFlightMode(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerTeleport(final PlayerTeleportEvent event) {
+        setSurvivalFlightMode(event.getPlayer());
+    }
+
+    private void setSurvivalFlightMode(final Player player) {
+        if (!player.getAllowFlight()
+                && player.getGameMode().equals(GameMode.SURVIVAL)
+                && player.hasPermission("monsterflight.fly")) {
+            player.setAllowFlight(true);
+        }
+    }
+
+}
